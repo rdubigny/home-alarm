0ffabf239b773c8957ea2178253f97e55a959ff3
Raphaël Dubigny <rdubigny@gmail.com>
2016-11-05 18:08:53 +0100

    remove bright pi

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 import time
 import threading
 
-from modules.brightpi import BrightPI
 from modules.camera import Camera
 from modules.door import Door
 from modules.light import Light
@@ -11,7 +10,6 @@ from modules.message import Message
 from modules.pirs import Pirs
 from modules.blue import Scanner
 
-bright_pi = BrightPI()
 camera = Camera()
 door = Door()
 light = Light()
@@ -21,7 +19,6 @@ scanner = Scanner()
 
 # check that everything is ok
 message.send_message_async('alarm started')
-bright_pi.turn_leds_on(mode='all')
 light.turn_on(5)
 camera.capture_and_upload_async()
 scanner.scan()
@@ -34,7 +31,6 @@ def main_loop():
         if scanner.is_armed:
             if pirs.is_detecting_move() or door.is_opened():
                 message.send_message_async('presence detected')
-                bright_pi.turn_leds_on(mode='ir_only')
                 camera.capture_and_upload_async()
             else:
                 if door.is_vibrating():
